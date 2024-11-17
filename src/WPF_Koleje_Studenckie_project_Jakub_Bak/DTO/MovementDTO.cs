e0f3f977ad96c9e8a935ddadf162c1e90a4fefc3
Jakub Bąk <jbjakubbak@gmail.com>
2024-11-17 20:07:08 +0100

    Refactor

---
+++ src/WPF_Koleje_Studenckie_project_Jakub_Bak/DTO/MovementDTO.cs
@@ -0,0 +1,10 @@
+using System.Text.Json.Serialization;
+
+namespace WPF_Koleje_Studenckie_project_Jakub_Bak.DTO
+{
+    public class MovementDTO
+    {
+        public int CurrentSpeed { get; set; }
+        public bool IsMoving { get; set; }
+    }
+}
