94ebb5288a829e44859aad332496b614ddef6e33
JakubBąk <143088151+Szeladin@users.noreply.github.com>
2024-12-08 11:52:10 +0100

    Delete src directory

--- src/Shared/Domain/Entities/TrainMovement.cs
+++
@@ -1,8 +0,0 @@
-namespace Domain.Entities
-{
-    public class TrainMovement
-    {
-        public int CurrentSpeed { get; set; } = 0;
-        public bool IsMoving { get; set; } = false;
-    }
-}
